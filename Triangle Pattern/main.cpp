0104f8b8e2ed5d1f0beb22a41d93cc3b340b937e
Abdul Moiz <abdulmoiz95972@gmail.com>
2024-11-08 22:38:13 +0500

    Done

---
+++ Triangle Pattern/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main(){
+	char ch='A';
+	int rows = 5;
+	for(int i=0; i<rows; i++){
+		for (int j=0; j<i+1; j++){
+			
+			cout<<ch;
+			ch++;
+		}
+		
+		
+		cout<<endl;
+	}
+	return 0;
+}
